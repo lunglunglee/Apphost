acb6dcece0242c5d0fbd0ed486845690e97db33e
lunglee <lunglungleelee@hotmail.com>
2016-06-08 16:46:31 +0800

    g

---
+++ WebApplication1/src/WebApplication1/Models/existing database/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.existing_database
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
